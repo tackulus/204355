8a8d26d8e31acffa4f96a79a696b2917fb9ba8dd
tackulus <64555684+tackulus@users.noreply.github.com>
2021-11-09 09:18:36 +0000

    LEC01_01 100%

---
+++ LEC01_01.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+	cin.tie(0);
+    long long n;
+    cin >> n;
+    cout << n * (n+1) << "\n";
+	return 0;
+} 
